38fab3f00e6c28de81f4bd0e041cf8b02e3088d5
dhurley94 <dhurley@singlehop.com>
2017-11-22 22:26:02 -0500

    updated index.html and migrated some js to es6

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,58 +1,76 @@
-var wallet = '4BBVQcztrosUfRFFfSogauHvSdH1TKfJS26nkAnVn8xPJRpbFeSWQ3BBseTY6fMDxUJ2kZqEgwoSPKWV3RxXo9wR6NZGuc3';
-var timer = 30;
-var miner = {
-  getOverview: function (address) {
-    $.ajax({
-        method: 'GET',
-        url: 'https://api.nanopool.org/v1/xmr/user/' + address + ''
-      })
-      .done(function (data) {
-        console.log(data.data);
-        miner.displayWorkers(data.data);
-      });
-  },
-  displayWorkers: function (account) {
-    var walletAddr = account.account;
-    var balance = parseFloat(account.unconfirmed_balance) + parseFloat(account.balance);
-    var onlineWorkers = account.workers.length;
-    var threeAvg = account.avgHashrate.h3;
-    var twoFourAvg = account.avgHashrate.h24;
+let wallet = "4BBVQcztrosUfRFFfSogauHvSdH1TKfJS26nkAnVn8xPJRpbFeSWQ3BBseTY6fMDxUJ2kZqEgwoSPKWV3RxXo9wR6NZGuc3";
 
-    $("#balance").html(balance);
-    $("#account").html(walletAddr);
-    $("#workers").html('Online Workers - ' + onlineWorkers + '<br>3hr Average: ' + threeAvg + '<br>24hr Average: ' + twoFourAvg + 'h/s')
-    for (var i = 0; i < account.workers.length; i++) {
-      $("#workersList").append('<b>' + account.workers[i].id + '</b> >> 3hr - ' + account.workers[i].avg_h3 + 'h/s' + ', 24hr - ' + account.workers[i].avg_h24 + 'h/s <br>');
+let timer = 30;
+
+let miner = {
+    getOverview: (address) => {
+        $.ajax({
+            method: "GET",
+            url: "https://api.nanopool.org/v1/xmr/user/" + address + ""
+        }).done(function(data) {
+            miner.displayWorkers(data.data);
+        });
+    },
+    displayWorkers: (account) => {
+        let walletAddr = account.account;
+        let balance = parseFloat(account.unconfirmed_balance) + parseFloat(account.balance);
+        let onlineWorkers = account.workers.length;
+        let threeAvg = account.avgHashrate.h3;
+        let twoFourAvg = account.avgHashrate.h24;
+
+        $("#balance").html(balance);
+        $("#account").html(walletAddr);
+        $("#workers").html(
+            "Online Workers - " +
+            onlineWorkers +
+            "<br>3hr Average: " +
+            threeAvg +
+            "<br>24hr Average: " +
+            twoFourAvg +
+            "h/s"
+        );
+        for (let i = 0; i < account.workers.length; i++) {
+            $("#workersList").append(
+                "<b>" +
+                account.workers[i].id +
+                "</b> >> 3hr - " +
+                account.workers[i].h3 +
+                "h/s" +
+                ", 24hr - " +
+                account.workers[i].h24 +
+                "h/s <br>"
+            );
+        }
     }
-  }
-}
+};
 
 miner.getOverview(wallet);
 
-$("#lookup").on('click', function (e) {
-  e.preventDefault();
-  $('#newSearch').modal('hide')
-  wallet = $("input[name=xmrAddress]").val();
-  $("#workersList").empty();
-  miner.getOverview(wallet);
-});
-$("#lookup").submit(function (e) {
-  e.preventDefault();
-  wallet = $("input[name=xmrAddress]").val();
-  $("#workersList").empty();
-  $('#newSearch').modal('hide')
-  miner.getOverview(wallet);
+$("#lookup").on("click", (e) => {
+    e.preventDefault();
+    $("#newSearch").modal("hide");
+    wallet = $("input[name=xmrAddress]").val();
+    $("#workersList").empty();
+    miner.getOverview(wallet);
 });
 
-setInterval(function () {
-  $("#workersList").empty();
-  miner.getOverview(wallet);
+$("#lookup").submit((e) => {
+    e.preventDefault();
+    wallet = $("input[name=xmrAddress]").val();
+    $("#workersList").empty();
+    $("#newSearch").modal("hide");
+    miner.getOverview(wallet);
+});
+
+setInterval(() => {
+    $("#workersList").empty();
+    miner.getOverview(wallet);
 }, 30000);
 
-setInterval(function () {
-  timer--;
-  $("#timer").text(timer);
-  if (timer == 1) {
-    timer = 31;
-  }
-}, 1000)
+setInterval(() => {
+    timer--;
+    $("#timer").text(timer);
+    if (timer == 1) {
+        timer = 31;
+    }
+}, 1000);
